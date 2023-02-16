51361fd6dbffbd3f425ba4d07527b69aa287cbcb
Cydonia8 <holymustaine20@gmail.com>
2023-02-16 14:20:01 +0100

    ultimos juegos y formulario contacto

--- controladores/ultimos_juegos.php
+++ controladores/ultimos_juegos.php
@@ -11,24 +11,5 @@
         $datos[$i] = juego::ultimosJuegos($plataforma);
         $i++;
     }
-    // foreach($datos as $matriz){
-    //     foreach($matriz as $array){
-    //         echo "<h2>$array[juego]</h2>";
-    //         echo "<h2>$array[plataforma]</h2>";
-    //         echo "<h2>$array[caratula]</h2>";
-    //         echo "<h2>$array[fecha]</h2>";
-    //         if(next($array) == false){
-    //             echo "<h3>zi</h3>";
-    //         }
-
-
-    //     }
-    // }
-    
-
-    // $datos_ps5 = juego::ultimosJuegos("PlayStation 5");
-    // $datos_switch = juego::ultimosJuegos("Nintendo Switch");
-    // $datos_xbox = juego::ultimosJuegos("Xbox Series X");
-    // $datos_ps2 = juego::ultimosJuegos("PlayStation 2");
     include "vistas/recientes.php";
 ?>
